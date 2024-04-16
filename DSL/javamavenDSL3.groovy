47cbf1133aac86c43d761eca7d3eae7e987b3d66
juliocarranza <jcarranza@credicorpcapital.com>
2024-04-15 20:58:06 -0500

    Cambios Archivo3

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/julioraulcarranzaruiz/dsljavamaven.git', 'master') { node ->
+            node / gitConfigName('julioraulcarranzaruiz')
+            node / gitConfigEmail('julioraulcarranzaruiz@gmail.com')
         }
     }
     triggers {
